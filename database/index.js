95ab4d38bcb852fd8340688b0514ff7f23e67fa1
DanyloKopytko <leonelluson@gmail.com>
2022-04-16 15:01:11 +0300

    SSL configs

--- database/index.js
+++ database/index.js
@@ -9,10 +9,16 @@ require('dotenv').config()
 
 const { seedInitialData } = require('../utils/testData/generateData')
 
+const ssl = process.env.SSL === 'true' ? { required: true, rejectUnauthorized: false } : false
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: 5432,
   dialect: 'postgres',
+  ssl: process.env.SSL === 'true',
+  dialectOptions: {
+    ssl,
+  },
 })
 
 // Load each model file
